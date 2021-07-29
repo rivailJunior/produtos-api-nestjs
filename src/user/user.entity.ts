7a42e553aafa209f9354be16393ef05fc98a0e48
rivail.santos <rivail.santos@olist.com>
2021-07-29 12:06:55 -0400

    adding validations

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,34 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
 export class User {
   id: number;
+
+  @IsNotEmpty({
+    message: 'Nome do usuário é obrigatório.',
+  })
+  @IsString({
+    message: 'Nome do usuário precisa ser uma string.',
+  })
   name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'Email precisa ser um endereço de email válido.',
+    },
+  )
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({
+    message: 'Senha é obrigatório.',
+  })
   password: string;
+
+  @IsNotEmpty({
+    message: 'Nome completo é obrigatorio!',
+  })
   fullname: string;
+
   registeredAt: Date;
 }

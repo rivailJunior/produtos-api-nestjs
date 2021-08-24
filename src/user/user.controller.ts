cfdb18086eb8ae57fe1c9f3bb1fd9335e5e86cbb
rivailjunior <rivail.rj@gmail.com>
2021-08-24 18:22:17 -0400

    adding exception to findUserByName

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,14 @@
 import { NestResponseBuilder } from './../core/http/nest-response.builder';
 import { User } from './user.entity';
-import { Controller, Post, Body, Get, Param, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { NestResponse } from 'src/core/http/nest-response';
 
@@ -11,6 +19,11 @@ export class UserController {
   @Get(':name')
   public findUserByName(@Param('name') name: string): User {
     const foundUser = this.userService.findUserByName(name);
+    if (!foundUser)
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'User not found',
+      });
     return foundUser;
   }
 
